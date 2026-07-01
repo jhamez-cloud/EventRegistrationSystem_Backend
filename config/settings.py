3a6260f925f496546becf8231f4ff3818780e9a0
jhamez-cloud <jameskekeli12@gmail.com>
2026-07-01 08:13:44 +0000

    update: configure rest_framework settings

--- config/settings.py
+++ config/settings.py
@@ -197,3 +197,13 @@ UNFOLD = {
         ],
     },
 }
+
+# REST Framework Settings
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
